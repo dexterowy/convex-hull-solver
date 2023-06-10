24850415caacd57d80899bf63b1625e79c7af981
Hubert Olszewski <holszewski@consorg.pl>
2023-06-10 22:49:34 +0200

    Add a convex hull algorithm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { ConvexHullAlgorithm, Point } from "./lib/ConvexHullAlgorithm";
+
 function App() {
+  const points: Point[] = [
+    { x: -2, y: 5 },
+    { x: 0, y: 9 },
+    { x: 2, y: 3 },
+    { x: 1, y: 1 }
+  ];
+
+  const convexHullAlgorithm = new ConvexHullAlgorithm(points);
+  convexHullAlgorithm.execute();
+
   return (
     <div className="App">
-      HELLO WORLD!
+      CONVEX-HULL-SOLVER
     </div>
   );
 }
